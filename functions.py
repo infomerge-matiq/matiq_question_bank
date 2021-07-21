967abdae885b6cc2d3c58c4e85f8153a8fe20693
Ali Sever <alisever96@hotmail.com>
2021-07-21 13:17:19 +0100

    Updated question validator.

--- functions.py
+++ functions.py
@@ -1,17 +1,23 @@
 import random
 
 
-def multiple_choice(question: str, choices: list[str], onepar=False):
+def multiple_choice(question: str, choices: list[str], correct: str,
+                    onepar=True, reorder=True):
+    """Takes question, choices(which contains answer), answer and returns
+    a multiple choice question and the answer"""
     layout = ""
     if onepar:
         layout = "onepar"
     choices_list = []
-    random.shuffle(choices)
+    if reorder:
+        random.shuffle(choices)
+    letter = "ABCDEFGHIJ"[choices.index(correct)]
     for choice in choices:
         choices_list.append(f"\\choice {choice}\n")
-    full_question = "".join([question, f"\n\n\\begin{{{layout}choices}}\n"] +
-                            choices_list + [f"\\end{{{layout}choices}}"])
-    return full_question
+    full_question = "".join(
+        [question, f"\n\n\\begin{{{layout}choices}}\n"] +
+        choices_list + [f"\\end{{{layout}choices}}"])
+    return [full_question, f"{letter}. {correct}"]
 
 
 def dollar(x):
@@ -91,14 +97,40 @@ def fraction_subtraction(a, b, c, d):
     return fraction_simplify(numerator, denominator)
 
 
+def valid_metric(unit):
+    prefixes = ["m", "c", "d", "", "da", "h", "k"]
+    base_units = ["m", "g", "l"]
+    return all([unit[-1] in base_units, unit[0:-1] in prefixes])
+
+
 def convert_measurement(number, unit_in, unit_out):
     prefixes = {"m": 0.001, "c": 0.01, "d": 0.1, "": 1, "da": 10,
                 "h": 100, "k": 1000}
-    base_units = ["m", "g", "l"]
     for unit in [unit_in, unit_out]:
-        if not unit[-1] in base_units or not unit[0:-1] in prefixes:
+        if not valid_metric(unit):
             raise NameError(f"{unit} is not a valid unit.")
     if unit_in[-1] != unit_out[-1]:
         raise TypeError("Units are not of the same type.")
     else:
         return number * prefixes[unit_in[0:-1]] / prefixes[unit_out[0:-1]]
+
+
+def convert_imperial(unit_in, unit_out, number=1):
+    if unit_in in ["inch", "inches"]:
+        return convert_measurement(number * 2.5, "cm", unit_out)
+    elif unit_in in ["lb", "lbs", "pounds"]:
+        return convert_measurement(number / 2.2, "kg", unit_out)
+    elif unit_in in ["pint", "pints"]:
+        return convert_measurement(number * 568, "ml", unit_out)
+    elif unit_in in ["mile", "miles"]:
+        return convert_measurement(number * 1.6, "km", unit_out)
+    elif unit_out in ["inch", "inches"]:
+        return convert_measurement(number / 2.5, unit_in, "cm")
+    elif unit_out in ["lb", "lbs", "pounds"]:
+        return convert_measurement(number * 2.2, unit_in, "kg")
+    elif unit_out in ["pint", "pints"]:
+        return convert_measurement(number / 568, unit_in, "ml")
+    elif unit_out in ["mile", "miles"]:
+        return convert_measurement(number / 1.6, unit_in, "km")
+    else:
+        raise NameError("Given units are invalid.")
